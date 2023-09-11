b0013a200d60a81db47eaada02d00d9f13611322
FacuSichi <facusichi99@hotmail.com>
2023-09-11 13:22:25 -0300

    Pre Release NPM Branch

--- src/02-component-patterns/pages/ShoppingPage.tsx
+++ src/02-component-patterns/pages/ShoppingPage.tsx
@@ -1,6 +1,6 @@
-import { ProductButtons, ProductCard, ProductImage, ProductTitle } from "../components/"
+// import { ProductButtons, ProductCard, ProductImage, ProductTitle } from "../components/"
+import { ProductCard, ProductButtons, ProductImage, ProductTitle } from 'fsdev-product-card'
 import { products } from "../data/products";
-import '../styles/custom-styles.css';
 
 const product = products[0];
 
@@ -13,29 +13,17 @@ export const ShoppingPage = () => {
 
             <ProductCard
                 key={product.id} product={product}
-                className="bg-dark text-white"
                 initialValues={{
                     count: 4,
-                    // maxCount: 10,
+                    maxCount: 10,
                 }}
             >
                 {
                     ({ reset, count, isMaxCountReached, increaseBy, maxCount }) => (
                         <>
-                            <ProductImage className="custom-image" style={{ boxShadow: '10px 10px 10px rgba(0,0,0,0.2' }} />
-                            <ProductTitle className="text-bold" />
-                            <ProductButtons className="custom-buttons" />
-
-                            <button onClick={reset}>  Reset </button>
-
-                            <button onClick={() => increaseBy(-2)}> -2 </button>
-
-                            {
-                                !isMaxCountReached &&
-                                <button onClick={() => increaseBy(2)}> +2 </button>
-                            }
-
-                            <span>{count} - {maxCount}</span>
+                            <ProductImage />
+                            <ProductTitle />
+                            <ProductButtons />
                         </>
                     )
                 }
